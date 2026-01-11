a953921bcb6fa95007104030edfb95e37aac3650
PARAS-KALURA <112518636+PARAS-KALURA@users.noreply.github.com>
2026-01-12 00:36:07 +0530

    mostly complet

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const items = []; // fake db
+
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+   res.send("API is working")
+});
+
+// get all items
+app.get("/items", (req, res) => {
+    res.json(items);
+});
+
+//Add new items
+app.post("/items", (req, res) => {
+    const newItem = req.body;
+    items.push(newItem);
+    res.status(201).json(newItem);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running ${PORT}`);
+})
