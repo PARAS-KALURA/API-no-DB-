2b520fb821b9305e72b6364f85821adf6b1e5bb6
PARAS-KALURA <112518636+PARAS-KALURA@users.noreply.github.com>
2026-01-12 01:11:04 +0530

    API Done

--- index.js
+++ index.js
@@ -19,6 +19,11 @@ app.get("/items", (req, res) => {
 //Add new items
 app.post("/items", (req, res) => {
     const newItem = req.body;
+
+     if (!newItem || Object.keys(newItem).length === 0) {
+    return res.status(400).json({ message: "No data sent" });
+  }
+
     items.push(newItem);
     res.status(201).json(newItem);
 });
